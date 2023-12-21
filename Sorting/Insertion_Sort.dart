07f167d3118648550f7bbdfd3dbd9d43d24a9d66
Afrad Ahsan <afradahsan2002@gmail.com>
2023-12-21 10:25:01 +0530

    Bubble Sort

--- Sorting/Insertion_Sort.dart
+++ Sorting/Insertion_Sort.dart
@@ -1,14 +1,18 @@
 void main(){
-
+  List array = [3,5,7,7,2,8,6,1,4,6];
+  List result = insertionSort(array);
+  print(result);
 }
 
-void insertionSort(List array){
-  int current = 0;
-  for(int i=1; i<array.length-1; i++){
-    current =array[i];
+List insertionSort(List array){
+  for(int i=1; i<=array.length-1; i++){
+    int current = array[i];
     int j=i-1;
     while(j>=0 && array[j]>current){
-      array[j]=array[j+1];
+      array[j+1]=array[j];
+      j--;
     }
+    array[j+1]=current;
   }
+  return array;
 }
