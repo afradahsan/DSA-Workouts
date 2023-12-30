f7ebc9eeabd750531416a2e831eac4d7d83c4fb2
Afrad Ahsan <afradahsan2002@gmail.com>
2023-12-30 08:57:08 +0530

    BST INSERT

--- TREE/binarysearch_tree.dart
+++ TREE/binarysearch_tree.dart
@@ -23,11 +23,45 @@ class BST{
 
     while (true) {
       if(data<currentNode!.data!.toInt()){
+        if(currentNode.left ==null){
         currentNode.left = Node(data);
+        break;
       }
       else{
         currentNode = currentNode.left;
       }
+      }
+      else{
+        if(currentNode.right == null){
+          currentNode.right = Node(data);
+          break;
+        }
+        else{
+          currentNode = currentNode.right;
+        }
+      }
     }
   }
+
+  void displayInOrder() {
+    _inOrderTraversal(root);
+  }
+
+  void _inOrderTraversal(Node? root) {
+    if (root != null) {
+      _inOrderTraversal(root.left);
+      print(root.data);
+      _inOrderTraversal(root.right);
+    }
+  }
+}
+
+void main(){
+  BST bst = BST();
+
+  bst.insert(10);
+  bst.insert(5);
+  bst.insert(15);
+
+  bst.displayInOrder();
 }
